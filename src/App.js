ba1029a8ca17be44a65927a4fed912253d666cb9
CMLTTN <cemaldim@gmail.com>
2023-07-07 12:19:14 +0300

    starts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import react, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return <div className="App"></div>;
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=ankara&appid=8db096cd6c6f2e686d9209fbc0c41924`;
+
+  return <div className="App">
+aloo
+  </div>;
 }
 
 export default App;
