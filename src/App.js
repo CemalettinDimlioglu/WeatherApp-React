1ae7df2443aea484aaa56904318f8c9a4c56ef0d
CMLTTN <cemaldim@gmail.com>
2023-07-07 14:19:18 +0300

    delete api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import react, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=ankara&appid=8db096cd6c6f2e686d9209fbc0c41924`;
+
 
   return (
     <div className="app">
